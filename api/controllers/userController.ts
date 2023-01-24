50c0cb9a722be058dd90cc61d879d8303262805a
김승수 <admin@SeungSu.local>
2023-01-24 20:35:49 +0900

    FIX : 이미지 변경시 자동변경 추가, 로그인 토큰 리턴 수정

--- api/controllers/userController.ts
+++ api/controllers/userController.ts
@@ -3,11 +3,12 @@ import userService from "../services/userService";
 import { asyncErrorHandler } from "../middlewares/error";
 
 class user {
-  static googleLogin = (req: Request, res: Response) => {
-    const code: any = req.headers.authorization;
-    const accessToken = userService.googleLogin(code);
-
-    res.status(200).json({ accessToken });
-  };
+  static googleLogin = asyncErrorHandler(
+    async (req: Request, res: Response) => {
+      const code: any = req.headers.authorization;
+      const accessToken = await userService.googleLogin(code);
+      res.status(200).json({ accessToken });
+    }
+  );
 }
 export default user;
--- api/models/userDao.ts
+++ api/models/userDao.ts
@@ -6,14 +6,23 @@ class userDao {
     userEmail: string,
     userImage: string
   ) => {
-    return myDataSource.query(
-      `INSERT INTO users u(
-      u.name,
-      u.email,
-      u.profile_image)
+    await myDataSource.query(
+      `INSERT INTO users(
+      name,
+      email,
+      profile_image)
       VALUES
-      (?, ?, ?)`,
+      ($1, $2, $3)`,
       [userName, userEmail, userImage]
+    );
+    return await myDataSource.query(
+      `SELECT
+      id,
+      name,
+      email
+      FROM users
+      WHERE users.email = $1;`,
+      [userEmail]
     );
   };
 
@@ -21,11 +30,23 @@ class userDao {
     return myDataSource.query(
       `SELECT 
       u.id,
-    u.email
+    u.email,
+    u.profile_image
     FROM 
     users u
-    WHERE u.email = '?'`,
+    WHERE u.email = $1`,
       [userEmail]
+    );
+  };
+  static updateUserProfileImage = async (
+    userEmail: string,
+    userImage: string
+  ) => {
+    return myDataSource.query(
+      `UPDATE users SET
+  profile_image = $2
+  WHERE email = $1`,
+      [userEmail, userImage]
     );
   };
 }
